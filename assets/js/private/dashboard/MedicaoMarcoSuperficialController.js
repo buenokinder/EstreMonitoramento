aaa62ac8fff28403ac61d1180a84b3ca6ff6ed7a
Carlos Bueno <carlos@sennit.com.br>
2016-07-11 08:21:12 -0300

    Marco

--- assets/js/private/dashboard/MedicaoMarcoSuperficialController.js
+++ assets/js/private/dashboard/MedicaoMarcoSuperficialController.js
@@ -2,6 +2,13 @@
 
 app.controller('MedicaoMarcoSuperficialController', ['$scope', '$http',   function($scope, $http){
       $scope.showContent = function($fileContent){
+
+        var linhas = $fileContent.split('\n');
+        for(var i = 0;i < linhas.length;i++){
+            var linha = linhas[i];
+
+            console.log(lines[i])
+        }
         $scope.content = $fileContent;
     };
    
