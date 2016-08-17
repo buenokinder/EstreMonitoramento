1ae89d30dbf10ea7096c7f225c3bce6dbbc57d58
Michel Oliveira <michel.oliveira@sennit.com.br>
2016-08-17 18:11:09 -0300

    ajustes na tela pluviometria e vazão

--- api/controllers/PluviometriaVazaoController.js
+++ api/controllers/PluviometriaVazaoController.js
@@ -6,7 +6,29 @@
  */
 
 module.exports = {
-	
+	search: function(req, res){
+		var where = {};
+		
+		for(key in req.allParams()) {
+			if(req.param(key)==undefined)continue;
+
+			where[key] = req.param(key);
+		}
+
+		var sort = { ano:  'ASC', mes: 'ASC', dia:'ASC' };
+
+		PluviometriaVazao.find({ where: where, sort: sort})
+		.populate('usuario')
+		.populate('aterro')
+		.exec(function(err, ret){
+			if(err){
+				return res.negotiate(err);
+			}else{
+				res.json(ret);	
+			}
+		});
+	},
+
 	listDistinctAnos: function(req, res){
 		PluviometriaVazao.native(function(err,coll){
 		  coll.distinct("ano", function(err,result){
--- assets/js/private/dashboard/PluviometriaVazaoController.js
+++ assets/js/private/dashboard/PluviometriaVazaoController.js
@@ -1,23 +1,32 @@
 
-
 app.controller('PluviometriaVazaoController', ['$scope', '$http','$filter',   function($scope, $http, $filter){
   $scope.operacaoPluviometrias = []; 
   $scope.mes = { id: 1};
-  $scope.meses= [{ id: 1, name:'Janeiro'}, {id: 2, name:'Fevereiro'}, {id: 3, name:'Março'}, {id: 4, name:'Abril'}, {id: 5, name:'Maio'}, {id: 6, name:'Junho'}, {id: 7, name:'Julho'}, {id: 8, name:'Agosto'}, {id: 9, name:'Setembro'}, {id: 10, name:'Outubro'}, {id: 11, name:'Novembro'}, {id: 12, name:'Dezembro'}];;
+  $scope.meses= [{ id: 1, name:'Janeiro'}, {id: 2, name:'Fevereiro'}, {id: 3, name:'Março'}, {id: 4, name:'Abril'}, {id: 5, name:'Maio'}, {id: 6, name:'Junho'}, {id: 7, name:'Julho'}, {id: 8, name:'Agosto'}, {id: 9, name:'Setembro'}, {id: 10, name:'Outubro'}, {id: 11, name:'Novembro'}, {id: 12, name:'Dezembro'}];
   $scope.ano = { id: 2016};
   $scope.anos = ([]);
   $scope.aterros = [];
   $scope.usuarios = [];  
+  $scope.currentYear = (new Date()).getFullYear();
+  $scope.totalSent=0;
+  $scope.totalErrors =0 ;
 
-  $scope.changeAno = function(ano) {
-      $http.get('/PluviometriaVazao?where={ "mes": "' + $scope.mes.id + '","ano": "' + ano  + '"}').success(function(data) {
+  $scope.search = function() {
+      //$http.get('/PluviometriaVazao?sort=data ASC&where={ "mes": "' + $scope.mes.id + '","ano": "' + $scope.ano.id   + '"}').success(function(data) {
+      $http.get('/PluviometriaVazao/search?mes='+$scope.mes.id+'&ano='+$scope.ano.id).success(function(data) {
         $scope.operacaoPluviometrias = angular.fromJson(data);
       });
   };
 
   $scope.loadDistinctAnos = function(){
     $http.get('/PluviometriaVazao/listDistinctAnos').success(function(data) {
+      if(data.length==0){
+        $scope.anos =  [{id:$scope.currentYear}];  
+        return;
+      }
+
       $scope.anos = angular.fromJson(data);
+
     });
   };
 
@@ -26,16 +35,15 @@ app.controller('PluviometriaVazaoController', ['$scope', '$http','$filter',   fu
     $scope.loadAterros();
     $scope.loadUsuarios();
     $scope.usuario = window.SAILS_LOCALS;
+    $scope.ano = {id:$scope.currentYear};
     $scope.excel;
   }
-      
 
   $scope.loadAterros = function() {
     return $scope.aterros.length ? null : $http.get('/Aterro').success(function(data) {
       $scope.aterros = angular.fromJson(data); 
     });
   };
-
     
   $scope.loadUsuarios = function() {
     return $scope.usuarios.length ? null : $http.get('/Usuario').success(function(data) {
@@ -47,44 +55,74 @@ app.controller('PluviometriaVazaoController', ['$scope', '$http','$filter',   fu
     $('.dropify').dropify({
       messages: {
           default: 'Arraste seu Arquivo',
-          
       }
     });
     $('#modalPluviometria').openModal();
   };
 
-  $scope.loadFile = function() {
-    var totalSent=0;
-    var totalErrors =0 ;
-    var checkImport = function(totalToSend, totalSent, totalErrors){
-      if(totalSent==totalToSend){
-        Materialize.toast('Importação finalizada '+(totalErrors>0?'com erros.':'')+'!', 4000);
-        $scope.changeAno($scope.ano.id);
-      }  
-    };
+  function checkStatusImport(){
+    if($scope.totalSent==$scope.excel.length-1){
+      Materialize.toast('Importação finalizada '+($scope.totalErrors>0?'com erros.':'')+'!', 4000);
+      $scope.search();
+      $scope.totalSent=0;
+      $scope.totalErrors=0;
+    }  
+  }
 
+  $scope.getMedicao = function(medicao, callback){
+    $http.get('/PluviometriaVazao?where={ "dia": "' + medicao.dia + '","mes": "' + medicao.mes + '","ano": "' + medicao.ano   + '"}').success(function(data) {
+      callback(data.length>0?angular.fromJson(data[0]):null);
+    });
+  };
+  
+  $scope.removeMedicao = function(medicao, callback, calbackError){
+    $http.delete('/PluviometriaVazao/'+medicao.id, medicao).then(function(result) {
+      callback(angular.fromJson(result.data));
+    }, function(error){
+      calbackError(error);      
+    });
+  };
+
+  $scope.addMedicao = function(medicao) {
+
+    $http.post('/PluviometriaVazao', medicao).then( 
+      function(result){
+        $scope.totalSent+=1;
+        checkStatusImport();
+      }, function(error){
+        swal("Erro", "Ocorreu uma falha ao importar a medição do dia '" + medicao.data + "'. Verifique se já foi inserida uma medição para essa mesma data anteriormente. :(", "error");
+        $scope.totalSent+=1;
+        $scope.totalErrors+=1;
+        checkStatusImport();
+      });
+  };
+
+  $scope.loadFile = function() {
   	alasql('SELECT * FROM FILE(?,{headers:false})',[event],function(res){
   		$scope.excel = angular.fromJson(res);
+      $scope.addMedicoes($scope.excel);
+  	});
+  };
 
-      angular.forEach($scope.excel, function(registro, index){
+  $scope.addMedicoes = function (medicoes){
+      angular.forEach(medicoes, function(registro, index){
         if(index != 0)
         {
-          var registroPluviometria = { data: registro.A +'/' + $scope.mes.id + '/' + $scope.ano.id  , dia: registro.A, pluviometria: registro.B, vazao: registro.C, aterro: $scope.usuario._aterro, usuario: $scope.usuario._id, mes: $scope.mes.id , ano: $scope.ano.id };
-          $http.post('/PluviometriaVazao', registroPluviometria, 
-            function(result){
-              totalSent+=1;
-              checkImport($scope.excel.length-1, totalSent, totalErrors);
+          var medicao = { data: registro.A +'/' + $scope.mes.id + '/' + $scope.ano.id  , dia: registro.A, pluviometria: registro.B, vazao: registro.C, aterro: $scope.usuario._aterro, usuario: $scope.usuario._id, mes: $scope.mes.id , ano: $scope.ano.id };
 
-            }, function(error){
-              swal("Erro", "Ocorreu uma falha ao importar o registro do dia '" + registro.A + "' :(", "error");
-              totalSent+=1;
-              totalErrors+=1;
-              checkImport($scope.excel.length-1, totalSent, totalErrors);
+          $scope.getMedicao(medicao, function(result){
+            if(null==result){
+              $scope.addMedicao(medicao);
+            }else{
+              $scope.removeMedicao(result, function(){
+                  $scope.addMedicao(medicao);
+              }, function(){
+                swal("Erro", "Ocorreu uma falha ao atualizar a medição do dia '" + medicao.dia + "/" + medicao.mes + "/" + medicao.ano + "'. :(", "error");      
+              });
             }
-          );
+          });
         }
-      });
-  	});
+      });    
   };
 
   $scope.showAterro = function(aterro) {
@@ -97,14 +135,9 @@ app.controller('PluviometriaVazaoController', ['$scope', '$http','$filter',   fu
   };
 
   $scope.showUsuario = function(user) {
-
     
     if(user && $scope.usuarios.length) {
-      console.log({id: user.usuario.id});
-      console.log($scope.usuarios);
-      
       var selected = $filter('filter')($scope.usuarios, {id: user.usuario.id});
-      console.log(selected);
       return selected.length ? selected[0].name : 'Not set';
     } else {
       return user.name || 'Not set';
@@ -118,16 +151,105 @@ app.controller('PluviometriaVazaoController', ['$scope', '$http','$filter',   fu
     }
   };
 
-  $scope.savePluviometria = function(data, id) {
+  $scope.savePluviometria = function(rowform, index, data, id) {
 
     angular.extend(data, {id: id});
-    console.log(data);
-    return $http.post('/PluviometriaVazao', data);
+    angular.extend(data, {usuario: $scope.usuario._id});
+    angular.extend(data, {aterro: $scope.usuario._aterro});
+
+    var medicao = { data: data.dia +'/' + $scope.mes.id + '/' + $scope.ano.id  , dia: data.dia, pluviometria: data.pluviometria, vazao: data.vazao, aterro: $scope.usuario._aterro, usuario: $scope.usuario._id, mes: $scope.mes.id , ano: $scope.ano.id };
+    if(undefined==id){
+
+      swal({  title: "",   
+              text: "Você tem certeza que deseja inserir a medição ?",   
+              type: "warning",   
+              showCancelButton: true, 
+              confirmButtonText: "Sim",   
+              cancelButtonText: "Cancelar",   
+              closeOnConfirm: false,   
+              closeOnCancel: false 
+          },function(isConfirm){   
+                  if (isConfirm) {    
+                    $scope.getMedicao(medicao, function(result){
+                      if(null==result){
+                        $http.post('/PluviometriaVazao', medicao).then(function(){
+                          swal("Registro Inserido!", "Seu registro foi inserido com sucesso.", "success");
+                          Materialize.toast('Registro inserido com sucesso!', 4000);                       
+                        }, function(error){
+                          swal("Erro", "Seu registro não foi inserido :(", "error");  
+                        });       
+                      }else{
+                        swal("Erro", "Já existe uma medição para essa mesma data. :(", "error");
+                        rowform.$cancel();
+                        $scope.operacaoPluviometrias.splice(index, 1);
+                        return false;
+                      }
+                    });    
+                  } else {
+                    swal("Cancelado", "Seu registro não foi editado :(", "error");
+                  } 
+            }); 
+
+
+
+    }else{
+
+      medicao.id = id;
+      swal({  title: "",   
+              text: "Você tem certeza que deseja editar a medição ?",   
+              type: "warning",   
+              showCancelButton: true, 
+              confirmButtonText: "Sim",   
+              cancelButtonText: "Cancelar",   
+              closeOnConfirm: false,   
+              closeOnCancel: false 
+          },function(isConfirm){   
+                  if (isConfirm) {    
+                    $http.put('/PluviometriaVazao/'+medicao.id, medicao).then(function(){
+                      swal("Registro Editado!", "Seu registro foi editado com sucesso.", "success");
+                      Materialize.toast('Registro editado com sucesso!', 4000);                       
+                    }, function(error){
+                      swal("Erro", "Seu registro não foi editado :(", "error");  
+                    });       
+
+                  } else {
+                    swal("Cancelado", "Seu registro não foi editado :(", "error");
+                  } 
+            });        
+       
+    }
   };
 
 
   $scope.removePluviometria = function(index) {
-    $scope.operacaoPluviometrias.splice(index, 1);
+    
+    if($scope.operacaoPluviometrias[index].id==undefined){
+      $scope.operacaoPluviometrias.splice(index, 1);
+      return;
+    }
+
+    swal({  title: "",   
+              text: "Você tem certeza que deseja excluir a medição ?",   
+              type: "warning",   
+              showCancelButton: true, 
+              confirmButtonText: "Sim",   
+              cancelButtonText: "Cancelar",   
+              closeOnConfirm: false,   
+              closeOnCancel: false 
+          },function(isConfirm){   
+                  if (isConfirm) {     
+                    $scope.removeMedicao($scope.operacaoPluviometrias[index], function(d){
+                      $scope.operacaoPluviometrias.splice(index, 1);
+                      swal("Registro Removido!", "Seu registro foi removido com sucesso.", "success");
+                      Materialize.toast('Registro inserido com sucesso!', 4000);                        
+                    }, function(error){
+                        swal("Erro", "Seu registro não foi removido :(", "error");  
+                    });
+                  } else {
+                    swal("Cancelado", "Seu registro não foi removido :(", "error");
+                  } 
+            }
+      );      
   };
 
   $scope.addPluviometria = function() {
