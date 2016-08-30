753103e81fca3090a94725fd33e0d596c53f3234
Michel Oliveira <martins.olivera@gmail.com>
2016-08-30 16:47:41 -0300

    inclusao do setup no dashboard

--- assets/js/private/dashboard/DashboardController.js
+++ assets/js/private/dashboard/DashboardController.js
@@ -1,77 +1,80 @@
 
 app.filter("asDate", function () {
     return function (input) {
-      var d = new Date(input);
-      d.setDate(d.getDate() + 1);
-      return d;        
+        var d = new Date(input);
+        d.setDate(d.getDate() + 1);
+        return d;
     }
 });
 
-app.controller('DashboardController', [ '$scope', '$location', '$rootScope', function($scope, $location, $rootScope){  
-  $scope.pai = undefined;
-  $scope.pathname = undefined;
-  $scope.link = "";
+app.controller('DashboardController', ['$scope', '$location', '$rootScope', function ($scope, $location, $rootScope) {
+    $scope.pai = undefined;
+    $scope.pathname = undefined;
+    $scope.link = "";
 
-  $scope.$watch('$routeUpdate', function(){
-    $scope.link = $location.path();
-    $scope.alteraStatusBreadcrumbs($location.path());
-  });
- $scope.perfil = window.SAILS_LOCALS._perfil;
-  //$scope.isAdmin =  window.SAILS_LOCALS.me.isAdmin;
-  $scope.goto = function(path){
-    $location.path(path);
-    $scope.alteraStatusBreadcrumbs(path);
-  }
+    $http.get('/setup').success(function (response, status) {
+    });
 
-  $scope.alteraStatusBreadcrumbs = function(pathname) {    
-    switch(pathname) {
-      case "/OperacaoSecaoCorte":
-        $scope.pathname = "Seção de Corte";
-        $scope.pai = "Área de Trabalho";
-        break; 
-      case "/PluviometriaVazao":
-        $scope.pathname = "Pluviometria e Vazão";
-        $scope.pai = "Área de Trabalho";
-        break;
-      case "/Dashboard":
-        $scope.pathname = "Dashboard";
-        $scope.pai = undefined;
-        break;
-      case '/Alerta' :
-        $scope.pathname = "Alerta";
-        $scope.pai = "Dados Mestre";
-        break;
-      case '/Aterro' :
-        $scope.pathname = "Aterro";
-        $scope.pai = "Dados Mestre";
-        break;
-      case '/SecaoCorte' :
-        $scope.pathname = "Seção de Corte";
-        $scope.pai = "Dados Mestre";
-        break;
-      case '/Template' :
-        $scope.pathname = "Template";
-        $scope.pai = "Relatórios";
-        break;        
-      case '/MarcoSuperficial' :
-        $scope.pathname = "Marco Superficial e Inclinômetro";
-        $scope.pai = "Dados Mestre";
-        break;  
-      case '/Piezometro' :
-        $scope.pathname = "Piezômetro";
-        $scope.pai = "Dados Mestre";
-        break;
-      case '/MedicaoPiezometro' :
-        $scope.pathname = "Piezômetro";
-        $scope.pai = "Área de Trabalho";
-        break;        
-      case '/Usuario' :
-        $scope.pathname = "Usuário";
-        $scope.pai = "Administração";
-        break;
-      default:
-        $scope.pai = undefined;
-        $scope.pathname = undefined;        
-    }    
-  };  
+    $scope.$watch('$routeUpdate', function () {
+        $scope.link = $location.path();
+        $scope.alteraStatusBreadcrumbs($location.path());
+    });
+    $scope.perfil = window.SAILS_LOCALS._perfil;
+    //$scope.isAdmin =  window.SAILS_LOCALS.me.isAdmin;
+    $scope.goto = function (path) {
+        $location.path(path);
+        $scope.alteraStatusBreadcrumbs(path);
+    }
+
+    $scope.alteraStatusBreadcrumbs = function (pathname) {
+        switch (pathname) {
+            case "/OperacaoSecaoCorte":
+                $scope.pathname = "Seção de Corte";
+                $scope.pai = "Área de Trabalho";
+                break;
+            case "/PluviometriaVazao":
+                $scope.pathname = "Pluviometria e Vazão";
+                $scope.pai = "Área de Trabalho";
+                break;
+            case "/Dashboard":
+                $scope.pathname = "Dashboard";
+                $scope.pai = undefined;
+                break;
+            case '/Alerta':
+                $scope.pathname = "Alerta";
+                $scope.pai = "Dados Mestre";
+                break;
+            case '/Aterro':
+                $scope.pathname = "Aterro";
+                $scope.pai = "Dados Mestre";
+                break;
+            case '/SecaoCorte':
+                $scope.pathname = "Seção de Corte";
+                $scope.pai = "Dados Mestre";
+                break;
+            case '/Template':
+                $scope.pathname = "Template";
+                $scope.pai = "Relatórios";
+                break;
+            case '/MarcoSuperficial':
+                $scope.pathname = "Marco Superficial e Inclinômetro";
+                $scope.pai = "Dados Mestre";
+                break;
+            case '/Piezometro':
+                $scope.pathname = "Piezômetro";
+                $scope.pai = "Dados Mestre";
+                break;
+            case '/MedicaoPiezometro':
+                $scope.pathname = "Piezômetro";
+                $scope.pai = "Área de Trabalho";
+                break;
+            case '/Usuario':
+                $scope.pathname = "Usuário";
+                $scope.pai = "Administração";
+                break;
+            default:
+                $scope.pai = undefined;
+                $scope.pathname = undefined;
+        }
+    };
 }]);
