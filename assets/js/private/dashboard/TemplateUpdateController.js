841a9a297e20ca3b641290b3d311ec94f5472498
Michel dos Prazeres <martins.olivera@gmail.com>
2016-09-19 11:36:08 -0300

    ajustes relatorios

--- assets/js/private/dashboard/TemplateUpdateController.js
+++ assets/js/private/dashboard/TemplateUpdateController.js
@@ -1,209 +1,223 @@
 app.controller('TemplateUpdateController', ['$location', '$routeParams', '$scope', '$http', '$compile', function ($location, $routeParams, $scope, $http, $compile) {
 
-	$scope.init = function () {
-		$scope.inputClass = "active";
-		$http.get("/Template/" + $routeParams.id).then(function (results) {
-			$scope.data = angular.fromJson(results.data);
-			$scope.paginas = angular.fromJson(results.data.paginas);
-			$scope.corpo = $scope.data.corpo;
-			$('.modal-trigger').leanModal();
-		});
-	};
+    $scope.init = function () {
+        $scope.inputClass = "active";
+        $http.get("/Template/" + $routeParams.id).then(function (results) {
+            $scope.data = angular.fromJson(results.data);
+            $scope.paginas = angular.fromJson(results.data.paginas);
+            $scope.corpo = $scope.data.corpo;
+            $('.modal-trigger').leanModal();
+        });
+    };
 
 
-	$scope.corpo = "";
-	$scope.id = $routeParams.id;
-	$scope.paginas = ([]);
-	$scope.selectedPage = "";
-	$scope.editPageId = 0;
+    $scope.corpo = "";
+    $scope.id = $routeParams.id;
+    $scope.paginas = ([]);
+    $scope.selectedPage = "";
+    $scope.editPageId = 0;
 
 
-	$scope.editarPagina = function (pagina, index) {
-		console.log(index);
-		$scope.editPageId = index;
+    $scope.editarPagina = function (pagina, index) {
+        console.log(index);
+        $scope.editPageId = index;
 
-		$scope.selectedPage = pagina.conteudo;
-		if (!$scope.$$phase) {
-			$scope.$apply();
-		}
+        $scope.selectedPage = pagina.conteudo;
+        if (!$scope.$$phase) {
+            $scope.$apply();
+        }
 
-	};
-	$scope.deletePagina = function (pagina) {
-		swal({
-			title: "Você tem certeza que deseja excluir?",
-			text: "Não será mais possivel recuperar esse Registro!",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonColor: "#DD6B55",
-			confirmButtonText: "Sim",
-			cancelButtonText: "Cancelar",
-			closeOnConfirm: false,
-			closeOnCancel: false
-		},
+    };
+    $scope.deletePagina = function (pagina) {
+        swal({
+            title: "Você tem certeza que deseja excluir?",
+            text: "Não será mais possivel recuperar esse Registro!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Sim",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
 			function (isConfirm) {
-				if (isConfirm) {
-					swal("Deletado!", "Seu registro foi excluido.", "success");
-					$http.delete('/Pagina/' + pagina.id)
+			    if (isConfirm) {
+			        swal("Deletado!", "Seu registro foi excluido.", "success");
+			        $http.delete('/Pagina/' + pagina.id)
 						.then(function (project) {
-							var index = $scope.paginas.indexOf(pagina);
-							$scope.paginas.splice(index, 1);
+						    var index = $scope.paginas.indexOf(pagina);
+						    $scope.paginas.splice(index, 1);
 
 						});
-				} else {
-					swal("Cancelado", "Seu registro está seguro :)", "error");
-				}
+			    } else {
+			        swal("Cancelado", "Seu registro está seguro :)", "error");
+			    }
 			}
 		);
-	}
-	$scope.savePage = function () {
-		swal({
-			title: "",
-			text: "Você tem certeza que deseja alterar este registro?",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonText: "Sim",
-			cancelButtonText: "Cancelar",
-			closeOnConfirm: false,
-			closeOnCancel: false
-		},
+    }
+    $scope.savePage = function () {
+        swal({
+            title: "",
+            text: "Você tem certeza que deseja alterar este registro?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Sim",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
 			function (isConfirm) {
-				if (isConfirm) {
+			    if (isConfirm) {
 
-					angular.forEach($scope.paginas, function (value, key) {
-						$http({
-							method: 'PUT',
-							url: '/Pagina/' + value.id,
-							data: value
-						}).then(function onSuccess(sailsResponse) {
+			        angular.forEach($scope.paginas, function (value, key) {
+			            $http({
+			                method: 'PUT',
+			                url: '/Pagina/' + value.id,
+			                data: value
+			            }).then(function onSuccess(sailsResponse) {
 
-							swal("Registro Alterado!", "Seu registro foi alterado com sucesso.", "success");
-							Materialize.toast('Registro alterado com sucesso!', 4000);
+			                swal("Registro Alterado!", "Seu registro foi alterado com sucesso.", "success");
+			                Materialize.toast('Registro alterado com sucesso!', 4000);
 
-						})
+			            })
 							.catch(function onError(sailsResponse) {
 
 							})
 							.finally(function eitherWay() {
-								$scope.sennitForm.loading = false;
+							    $scope.sennitForm.loading = false;
 							})
 
-					});
+			        });
 
-				} else {
-					swal("Cancelado", "Seu registro não foi alterado :(", "error");
-				}
+			    } else {
+			        swal("Cancelado", "Seu registro não foi alterado :(", "error");
+			    }
 			}
 		);
 
 
-	}
-	$scope.novaPagina = function () {
-		var model = {
-			conteudo: '',
-			pagina: 0,
-			nome: 'Nova Pagina',
-			template: $routeParams.id
+    }
+    $scope.novaPagina = function () {
+        var model = {
+            conteudo: '',
+            pagina: 0,
+            nome: 'Nova Pagina',
+            template: $routeParams.id
 
-		}
-		if ($scope.paginas.length > 0) {
-			model =
+        }
+        if ($scope.paginas.length > 0) {
+            model =
 				{
-					conteudo: '',
-					pagina: $scope.paginas.length + 1,
-					nome: 'Nova Pagina',
-					template: $routeParams.id
+				    conteudo: '',
+				    pagina: $scope.paginas.length + 1,
+				    nome: 'Nova Pagina',
+				    template: $routeParams.id
 
 				};
 
-		}
-		$http.post('/Pagina', model).then(
+        }
+        $http.post('/Pagina', model).then(
 			function (result) {
-				console.log('retorno');
-				console.log(result);
-				$scope.paginas.push(result.data);
+			    console.log('retorno');
+			    console.log(result);
+			    $scope.paginas.push(result.data);
 			}, function (error) {
-				swal("Erro", "Erro ao incluir página", "error");
+			    swal("Erro", "Erro ao incluir página", "error");
 
 			});
 
 
-		console.log($scope.paginas);
+        console.log($scope.paginas);
 
-	}
+    }
 
 
 
 
     String.prototype.replaceAll = function (s, r) { return this.split(s).join(r) }
 
-	$scope.componenteNome = "";
-	$scope.componenteTipo = "";
-	$scope.componenteGrafico = "";
-	$scope.componenteTabela = "";
-	$scope.componenteValor = "";
+    $scope.componenteNome = "";
+    $scope.componenteTipo = "";
+    $scope.componenteGrafico = "";
+    $scope.componenteTabela = "";
+    $scope.componenteValor = "";
 
-	$scope.addComponente = function (value) {
-		console.log('teste');
+    $scope.addComponente = function (value) {
 
-		console.log($scope.componenteTipo);
-		if ($scope.componenteTipo == 'tabela') {
-			console.log($scope.componenteTipo);
-			$scope.selectedPage += "<tabela tipo='" + $scope.componenteTabela + "'  aterro='" + $scope.data.aterro.id + "'  inicio='" + $scope.data.dataInicial + "'  fim='" + $scope.data.dataFim + "'><p style='color: red;' >" + $scope.componenteNome + "</p></tabela>"
-		} else {
-			$scope.selectedPage += "<graficohorizontal  tipado='" + $scope.componenteValor + "' aterro='" + $scope.data.aterro.id + "'  inicio='" + $scope.data.dataInicial + "'  fim='" + $scope.data.dataFim + "'  ><p style='color: red;' >" + $scope.componenteNome + "</p></graficohorizontal>";
+        if ($scope.componenteNome == "") {
+            swal("Erro", "Para prosseguir é necessário que informe um label para " + ($scope.componenteTipo == 'tabela' ? 'a tabela' : 'o gráfico'), "error");
+            $(".lean-overlay").hide();
+            return;
+        }
 
-		}
-		console.log($scope.selectedPage);
-		$(".lean-overlay").hide();
-	}
-	$scope.preparaConteudo = function (value) {
+        if ($scope.componenteTipo == "?") {
+            swal("Erro", "Para prosseguir é necessário que selecione o tipo do componente.", "error");
+            $(".lean-overlay").hide();
+            return;
+        }
 
-		var respostas = value.split('{{');
-		function myFunction(item, index) {
-			if (index != 0) {
-				var tipo = item.split('}}')[0];
-				if (tipo.indexOf('tabela(') !== -1) {
-					var parametro = tipo.split('&#39;')[1];
+        if ($scope.componenteTipo == 'tabela') {
+            if ($scope.componenteTabela == "") {
+                swal("Erro", "Para prosseguir é necessário que selecione a tabela.", "error");
+                $(".lean-overlay").hide();
+                return;
+            }
+            $scope.selectedPage += "<tabela tipo='" + $scope.componenteTabela + "'  aterro='" + $scope.data.aterro.id + "'  inicio='" + $scope.data.dataInicial + "'  fim='" + $scope.data.dataFim + "'><p style='color: red;' >" + $scope.componenteNome + "</p></tabela>"
+        } else {
 
+            $scope.selectedPage += "<graficohorizontal  tipado='" + $scope.componenteValor + "' aterro='" + $scope.data.aterro.id + "'  inicio='" + $scope.data.dataInicial + "'  fim='" + $scope.data.dataFim + "'  ><p style='color: red;' >" + $scope.componenteNome + "</p></graficohorizontal>";
+        }
+        $(".lean-overlay").hide();
+    };
 
-					value = value.replaceAll('{{' + item.split('}}')[0] + '}}', '<tabela tipo=\'' + parametro + '\' aterro=\'' + $scope.aterro + '\'   inicio=\'' + $scope.data.dataInicial + '\' fim=\'' + $scope.data.dataFim + '\' ></tabela>  ');
-				}
-				if (tipo.indexOf('grafico(') !== -1) {
-					console.log('entrou 1');
-					var parametro = tipo.split('&#39;')[1];
+    $scope.preparaConteudo = function (value) {
 
-
-					var parametro2 = tipo.split('&#39;')[3];
-					console.log('entrou 2');
-					if (parametro == 'marcohorizontal') {
-						console.log('entrou 3');
-						value = value.replaceAll('{{' + item.split('}}')[0] + '}}', '<graficohorizontal  tipado=\'' + parametro2 + '\'  aterro=\'' + $scope.aterro + '\'  inicio=\'' + $scope.data.dataInicial + '\' fim=\'' + $scope.data.dataFim + '\'  ></graficohorizontal>  ');
-					} else {
-						console.log('entrou 4');
-						value = value.replaceAll('{{' + item.split('}}')[0] + '}}', '<graficovertical tipado=\'' + parametro2 + '\'  aterro=\'' + $scope.aterro + '\'  inicio=\'' + $scope.data.dataInicial + '\' fim=\'' + $scope.data.dataFim + '\'  ></graficovertical>  ');
-					}
-
-				}
-			}
-		}
+        var respostas = value.split('{{');
+        function myFunction(item, index) {
+            if (index != 0) {
+                var tipo = item.split('}}')[0];
+                if (tipo.indexOf('tabela(') !== -1) {
+                    var parametro = tipo.split('&#39;')[1];
 
 
-		respostas.forEach(myFunction);
-		console.log(value);
-		return value;
-	}
-	$scope.verPagina = function () {
-		console.log('teste');
-		console.log($scope.selectedPage);
+                    value = value.replaceAll('{{' + item.split('}}')[0] + '}}', '<tabela tipo=\'' + parametro + '\' aterro=\'' + $scope.aterro + '\'   inicio=\'' + $scope.data.dataInicial + '\' fim=\'' + $scope.data.dataFim + '\' ></tabela>  ');
+                }
+                if (tipo.indexOf('grafico(') !== -1) {
+                    console.log('entrou 1');
+                    var parametro = tipo.split('&#39;')[1];
 
 
-		$scope.paginas[$scope.editPageId].conteudo = $scope.selectedPage;
-		console.log($scope.paginas);
+                    var parametro2 = tipo.split('&#39;')[3];
+                    console.log('entrou 2');
+                    if (parametro == 'marcohorizontal') {
+                        console.log('entrou 3');
+                        value = value.replaceAll('{{' + item.split('}}')[0] + '}}', '<graficohorizontal  tipado=\'' + parametro2 + '\'  aterro=\'' + $scope.aterro + '\'  inicio=\'' + $scope.data.dataInicial + '\' fim=\'' + $scope.data.dataFim + '\'  ></graficohorizontal>  ');
+                    } else {
+                        console.log('entrou 4');
+                        value = value.replaceAll('{{' + item.split('}}')[0] + '}}', '<graficovertical tipado=\'' + parametro2 + '\'  aterro=\'' + $scope.aterro + '\'  inicio=\'' + $scope.data.dataInicial + '\' fim=\'' + $scope.data.dataFim + '\'  ></graficovertical>  ');
+                    }
+
+                }
+            }
+        }
+
+
+        respostas.forEach(myFunction);
+        console.log(value);
+        return value;
+    }
+    $scope.verPagina = function () {
+        console.log('teste');
+        console.log($scope.selectedPage);
+
+
+        $scope.paginas[$scope.editPageId].conteudo = $scope.selectedPage;
+        console.log($scope.paginas);
 
     };
 
-	// $scope.getData() = function(){
-	// 	 function myFunction(item, index) {
+    // $scope.getData() = function(){
+    // 	 function myFunction(item, index) {
     //             if (index != 0) {
     //                 var tipo = item.split('}}')[0];
     //                 if (tipo.indexOf('tabela(') !== -1) {
@@ -228,49 +242,49 @@ app.controller('TemplateUpdateController', ['$location', '$routeParams', '$scope
     //             }
     //         }
     //         respostas.forEach(myFunction);
-	// }
-	$scope.init();
-	$scope.save = function () {
-		console.log('data editado', $scope.data)
+    // }
+    $scope.init();
+    $scope.save = function () {
+        console.log('data editado', $scope.data)
 
-		// $scope.sennitForm.loading = true;
-		swal({
-			title: "",
-			text: "Você tem certeza que deseja alterar este registro?",
-			type: "warning",
-			showCancelButton: true,
-			confirmButtonText: "Sim",
-			cancelButtonText: "Cancelar",
-			closeOnConfirm: false,
-			closeOnCancel: false
-		},
+        // $scope.sennitForm.loading = true;
+        swal({
+            title: "",
+            text: "Você tem certeza que deseja alterar este registro?",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Sim",
+            cancelButtonText: "Cancelar",
+            closeOnConfirm: false,
+            closeOnCancel: false
+        },
 			function (isConfirm) {
-				if (isConfirm) {
+			    if (isConfirm) {
 
-					$http({
-						method: 'PUT',
-						url: '/Template' + '/' + $scope.data.id,
-						data: $scope.data
-					}).then(function onSuccess(sailsResponse) {
-						$scope.inputClass = null;
-						//sennitCommunicationService.prepForBroadcastDataList($scope.data);
-						$scope.data = ([]);
-						$scope.inputClass = "disabled";
-						swal("Registro Alterado!", "Seu registro foi alterado com sucesso.", "success");
-						Materialize.toast('Registro alterado com sucesso!', 4000);
-						location.assign("#/Template");
-					})
+			        $http({
+			            method: 'PUT',
+			            url: '/Template' + '/' + $scope.data.id,
+			            data: $scope.data
+			        }).then(function onSuccess(sailsResponse) {
+			            $scope.inputClass = null;
+			            //sennitCommunicationService.prepForBroadcastDataList($scope.data);
+			            $scope.data = ([]);
+			            $scope.inputClass = "disabled";
+			            swal("Registro Alterado!", "Seu registro foi alterado com sucesso.", "success");
+			            Materialize.toast('Registro alterado com sucesso!', 4000);
+			            location.assign("#/Template");
+			        })
 						.catch(function onError(sailsResponse) {
 
 						})
 						.finally(function eitherWay() {
-							// $scope.sennitForm.loading = false;
+						    // $scope.sennitForm.loading = false;
 						})
-				} else {
-					swal("Cancelado", "Seu registro não foi alterado :(", "error");
-				}
+			    } else {
+			        swal("Cancelado", "Seu registro não foi alterado :(", "error");
+			    }
 			}
 		);
-	};
+    };
 
 }]);
