a1d747107cd7c7b30bcd4045cf7ede86335aaf4c
Michel dos Prazeres <martins.olivera@gmail.com>
2016-09-27 14:36:03 -0300

    merge

--- crontab/alertasMarcoSuperficial.js
+++ crontab/alertasMarcoSuperficial.js
@@ -355,10 +355,6 @@
 
     _inspectMonitoramentos: function (context, monitoramentos) {
         var dataBase = new Date();
-<<<<<<< HEAD
-=======
-      
->>>>>>> 2016_09_26__2016_09_30
 
         for (var i = 0; i < monitoramentos.length; i++) {
 
