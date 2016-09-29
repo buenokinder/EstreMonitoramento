5e768d515c78b9f91585c8db3a0fbea8655dc711
Michel dos Prazeres <martins.olivera@gmail.com>
2016-09-29 10:18:39 -0300

    ajustes homologacao. importacao de marco superficial

--- api/models/MarcoSuperficial.js
+++ api/models/MarcoSuperficial.js
@@ -4,8 +4,8 @@ module.exports = {
     attributes: {
         nome: {
             type: 'string',
-            required: true,
-            unique: true
+            required: true
+           // unique: true
         },
         norte: {
             type: 'string',
@@ -33,11 +33,38 @@ module.exports = {
         usuario: {
             model: 'Usuario'
         },
-        /*medicaoMarcoSuperficialDetalhes: {
-          collection: 'medicaoMarcoSuperficialDetalhes',
-          via: 'marcoSuperficial'
-        },*/
+        //compositePk: {
+        //    type: 'string',
+        //    unique: true
+        //},
         medicaoMarcoSuperficialDetalhes: {},
         deslocamentos: {}
+    },
+
+    beforeCreate: function (value, callback) {
+        MarcoSuperficial.findOne({ where: { nome: value.nome, aterro: value.aterro } }).exec(function (err, marcoSuperficial) {
+            if (marcoSuperficial == undefined) {
+                callback();
+            }
+            else {
+                callback("Já existe um marco superficial com o nome e aterro informados.");
+            }
+        });
     }
+
+    //beforeValidation: function (values, cb) {
+    //    if (values.aterro == undefined) {
+    //        cb();
+    //        return;
+    //    }
+
+    //    var aterro = "";
+    //    if (typeof values.aterro == "string")
+    //        aterro = values.aterro;
+    //    else
+    //        aterro = values.aterro.id;
+
+    //    values.compositePk = aterro + "|" + values.nome;
+    //    cb();
+    //}
 };
